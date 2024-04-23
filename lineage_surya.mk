1acba0dc2d1113b4622d07dc579570d6e7fce196
shoya0x00 <shoya0x00@gmail.com>
2024-04-24 01:30:21 +0600

    surya: override build description to match build fingerprint
    BUILD_FINGERPRINT(02065f6) Should fix the LOS based UQPR2 rom boot issue. to avoid future issues. adding prop override for PRIVATE_BUILD_DESC

--- lineage_surya.mk
+++ lineage_surya.mk
@@ -22,4 +22,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 BUILD_FINGERPRINT := POCO/surya_global/surya:12/RKQ1.211019.001/V14.0.2.0.SJGMIXM:user/release-keys
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="surya_global-user 12 RKQ1.211019.001 V14.0.2.0.SJGMIXM release-keys"
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
